6eabaf920e326ab32cd6873ef5f5302dff385939
Camila de_trincheria <cdetrincheria@hotmail.com>
2017-11-06 17:47:01 -0300

    Implement App class

--- src/main/java/ar/uba/fi/tdd/rulogic/App.java
+++ src/main/java/ar/uba/fi/tdd/rulogic/App.java
@@ -1,12 +1,28 @@
 package ar.uba.fi.tdd.rulogic;
+import ar.uba.fi.tdd.rulogic.model.KnowledgeBase;
+import java.io.*;
 
 /**
  * Console application.
  *
  */
 public class App
-{
-	public static void main(String[] args) {
-		System.out.println("I shall answer all your questions!");
+{	
+	public static void main (String[] args) throws Exception {
+		String pathToDatabaseFile = args[0];
+		String line = null;
+		KnowledgeBase knowledgeBase = new KnowledgeBase();
+		
+		knowledgeBase.parseDatabase(pathToDatabaseFile);
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Welcome! I shall answer all your questions!");
+		System.out.println("Please enter your query or type 'quit' to exit the application:");
+		line = bufferedReader.readLine();
+		while(!line.equals("quit")) {
+			boolean queryAnswer = knowledgeBase.answer(line);
+			System.out.println(queryAnswer);
+			System.out.println("Please enter your next query or type 'quit' to exit the application:");
+			line = bufferedReader.readLine();
+		}
     }
 }

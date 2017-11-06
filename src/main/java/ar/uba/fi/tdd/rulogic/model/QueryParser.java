da8c01b215fa4a805e74b00541226aa8662870a2
Camila de_trincheria <cdetrincheria@hotmail.com>
2017-11-06 17:37:26 -0300

    Include QueryParser

---
+++ src/main/java/ar/uba/fi/tdd/rulogic/model/QueryParser.java
@@ -0,0 +1,12 @@
+package ar.uba.fi.tdd.rulogic.model;
+import ar.uba.fi.tdd.rulogic.model.Parser;
+
+public class QueryParser extends Parser {
+
+	/* Returns the parameters of the given query. */
+    public String[] obtainQueryParameters(String query) {
+    	String cleanQuery = removeSpaces(query);
+        String queryParametersPattern = "^.*\\((.*)\\)$";
+        return obtainClauseParameters(cleanQuery, queryParametersPattern);
+    }
+}

3c74ff589cd2a9193efff081c7a9cc3ba136e6a7
Camila de_trincheria <cdetrincheria@hotmail.com>
2017-11-06 17:37:10 -0300

    Include FactParser

---
+++ src/main/java/ar/uba/fi/tdd/rulogic/model/FactParser.java
@@ -0,0 +1,12 @@
+package ar.uba.fi.tdd.rulogic.model;
+import ar.uba.fi.tdd.rulogic.model.Parser;
+
+public class FactParser extends Parser {
+
+	/* Returns the parameters of the given fact. */
+    public String[] obtainFactParameters(String fact) {
+    	String cleanFact = removeSpaces(fact);
+        String factParametersPattern = "^.*\\((.*)\\)\\.$";
+        return obtainClauseParameters(cleanFact, factParametersPattern);
+    }
+}
